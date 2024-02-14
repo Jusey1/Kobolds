5a69830bd5ac947153496dbce600028d58727911
Salju <61861078+Jusey1@users.noreply.github.com>
2024-02-14 15:12:03 -0500

    Add files via upload

--- src/main/java/net/salju/kobolds/init/KoboldsStructures.java
+++ src/main/java/net/salju/kobolds/init/KoboldsStructures.java
@@ -11,8 +11,7 @@ import net.minecraft.core.registries.Registries;
 
 public class KoboldsStructures {
 	public static final DeferredRegister<StructureType<?>> REGISTRY = DeferredRegister.create(Registries.STRUCTURE_TYPE, KoboldsMod.MODID);
-	public static final RegistryObject<StructureType<KoboldDenStructure>> KOBOLD_DEN = REGISTRY.register("kobold_den", () -> stuff(KoboldDenStructure.CODEC));
-	public static final RegistryObject<StructureType<KoboldSurfaceStructure>> KOBOLD_SURFACE = REGISTRY.register("kobold_surface", () -> stuff(KoboldSurfaceStructure.CODEC));
+	public static final RegistryObject<StructureType<AbstractKoboldStructure>> KOBOLD_DEN = REGISTRY.register("kobold_den", () -> stuff(AbstractKoboldStructure.CODEC));
 
 	private static <T extends Structure> StructureType<T> stuff(Codec<T> codec) {
 		return () -> codec;
